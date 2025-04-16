cce753fd7ed166e9f306d4fc81d1c402bddb178f
Krzysztof Szymański <kszymanski926@gmail.com>
2025-04-16 17:08:53 +0200

    feat: parametrise universal shift register

--- 06/UniversalShiftRegister.sv
+++ 06/UniversalShiftRegister.sv
@@ -13,13 +13,13 @@ endmodule
 // works by choosing the input signal for each flip-flop:
 // d for parallel loading, the older flip-flop's out for left loading,
 // the younger flip-flop for right loading, and itself for keeping the state
-module UniversalShiftRegister(
-    output [7:0] q, 
-    input  i, c, l, r, input [7:0] d
+module UniversalShiftRegister#(parameter N = 8)(
+    output [N-1:0] q, 
+    input  i, c, l, r, input [N-1:0] d
 );
-    logic [7:0] in, nq;
-    assign in = l ? (r ? d : {i, q[7:1]})
-                  : (r ? {q[6:0], i} : q);
-    for (genvar j = 0; j < 8; j = j + 1)
+    logic [N-1:0] in, nq;
+    assign in = l ? (r ? d : {i, q[N-1:1]})
+                  : (r ? {q[N-2:0], i} : q);
+    for (genvar j = 0; j < N; j = j + 1)
         DFlipFlop dff(q[j], nq[j], c, in[j]);
 endmodule

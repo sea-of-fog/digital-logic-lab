d6ea73a3bdb9296ca886df1cd46fa86529d01ef1
Krzysztof Szymański <kszymanski926@gmail.com>
2025-04-23 15:10:43 +0200

    refactor: document two's complement module, change c -> carry

--- 07/TwosComplement.sv
+++ 07/TwosComplement.sv
@@ -1,13 +1,16 @@
+// High ini means loading the number
+// The module does not know when to stop computing -- the user should 
+// set fin to high N + 1 cycles after ini
 module TwosComplement#(parameter N = 4)(
     output logic [N-1:0] neg, 
   	input  [N-1:0] d, input ini, fin, clk
 );
-    logic c;    
+    logic carry;    
     always_ff @(posedge clk)
       	if (ini) neg <= d;
   		else if (fin) neg <= neg;
-  		else neg <= {~neg[0] ^ c, neg[N-1:1]};
+  		else neg <= {~neg[0] ^ carry, neg[N-1:1]};
     always_ff @(posedge clk)
-        if (ini) c <= 1;
-        else c <= c && ~neg[0];
+        if (ini) carry <= 1;
+        else carry <= carry && ~neg[0];
 endmodule
